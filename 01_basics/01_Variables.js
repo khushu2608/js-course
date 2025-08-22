153d681260ec20605268d97f214496a2e035466e
Khushbu Singh <ks9565766@gmail.com>
2025-08-22 04:44:21 +0000

    Variables

---
+++ 01_basics/01_Variables.js
@@ -0,0 +1,6 @@
+const accountId = "122"
+let accountEmail = "ksjdh.gmailcom"
+let accountCity = "Bengluru"
+let y;
+console.log(accountEmail)
+console.table([accountCity, accountEmail, accountId,y])
