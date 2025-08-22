1a79bde8760e82000d0c9b4eb94b1ccb5028822e
Khushbu Singh <ks9565766@gmail.com>
2025-08-22 05:04:20 +0000

    dataTypes

---
+++ 01_basics/02_DataTypes.js
@@ -0,0 +1,19 @@
+"use strict";//treat all JS code as newer version
+
+//alert("Shri Radhey!") // WE are using Nodejs , not browser
+
+let age = 18
+let isLogged = false
+let state = null
+
+//number =>2^53
+//bigInt
+//String => ""
+//boolean=>true/false
+//null => standalone value
+//undefined
+//symbol=>unique
+
+//object
+
+console.log(typeof undefined)
