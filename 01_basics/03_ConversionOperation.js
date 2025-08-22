4a868e702de4d78db0ede864dd8914de0b0b8d22
Khushbu Singh <ks9565766@gmail.com>
2025-08-22 05:38:10 +0000

    String Operation

--- 01_basics/03_ConversionOperation.js
+++ 01_basics/03_ConversionOperation.js
@@ -1,16 +1,16 @@
 let score = null
 let score1 = undefined
-console.log(typeof score)
-console.log(typeof(score))
+// console.log(typeof score)
+// console.log(typeof(score))
 
 let valueInNumber = Number(score)
 let valueInNumber1 = Number(score1)
 
-console.log(typeof valueInNumber)
-console.log(valueInNumber)  // Not a Number
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)  // Not a Number
 
-console.log(typeof valueInNumbe1)
-console.log(valueInNumber1)
+// console.log(typeof valueInNumbe1)
+// console.log(valueInNumber1)
 
 //"33" => 33
 //"333qqw" => NaN
@@ -20,7 +20,7 @@ console.log(valueInNumber1)
 let isLoggedIn = undefined
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)  //O/P => true
+//console.log(booleanIsLoggedIn)  //O/P => true
 
 // 1=> true
 // "" => false; "Hitesh" => true
@@ -31,5 +31,20 @@ let someNumber = 33
 
 let stringNumber = String(someNumber)
 
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+// *****************  Operation  ***********************
+
+let value = 3
+let negValue = -value
+console.log(negValue)
+
+console.log(2**3)   // power =>**
+
+let str1 ="Hello"
+let str2 =" Hitesh"
+
+let str3=str1+str2
+
+console.log(str3)
